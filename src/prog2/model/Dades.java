ba00507422a8618306c829e38e4ea14f6c6e5a0e
guillemverdaguer <gverdabo7@alumnes.ub.edu>
2026-05-05 19:41:46 +0200

    semen

--- src/prog2/model/Dades.java
+++ src/prog2/model/Dades.java
@@ -11,10 +11,10 @@ public class Dades implements InDades, Serializable {
     private LlistaPrestecs llistaPrestecs;
     private LlistaUsuaris llistaUsuaris;
 
-    public Dades(LlistaExemplars llistaExemplars, LlistaPrestecs llistaPrestecs, LlistaUsuaris llistaUsuaris) {
-        this.llistaExemplars = llistaExemplars;
-        this.llistaPrestecs = llistaPrestecs;
-        this.llistaUsuaris = llistaUsuaris;
+    public Dades() {
+        this.llistaExemplars = new LlistaExemplars();
+        this.llistaPrestecs = new LlistaPrestecs();
+        this.llistaUsuaris = new LlistaUsuaris();
     }
 
     @Override
@@ -45,32 +45,65 @@ public class Dades implements InDades, Serializable {
     }
 
     @Override
-    public void afegirPrestec(int exemplarPos, int usuariPos, boolean esLlarg) throws BiblioException {
-        if(esLlarg && !(llistaExemplars.getAt(exemplarPos).getAdmetPrestecLlarg())) {
-            throw new BiblioException("L'exemplar no admet prestecs llargs");
-        }
-        if(!llistaExemplars.getAt(exemplarPos).isDisponible()){
-            throw  new BiblioException("L'exemplar no està disponible");
-        }
-        Date date;
+        public void afegirPrestec(int exemplarPos, int usuariPos, boolean esLlarg) throws BiblioException {
+            if(esLlarg && !(llistaExemplars.getAt(exemplarPos).getAdmetPrestecLlarg())) {
+                throw new BiblioException("L'exemplar no admet prestecs llargs");
+            }
+            if(!llistaExemplars.getAt(exemplarPos).isDisponible()){
+                throw  new BiblioException("L'exemplar no està disponible");
+            }
+            for(Prestec prestec : llistaPrestecs.getArrayList()){
+                if(prestec.getUsuari().equals(llistaUsuaris.getAt(usuariPos)) && prestec.prestecEndarrerit()){
+                    throw new BiblioException("L'usuari té prestecs endarrerits");
+                }
+            }
+            if(llistaUsuaris.getAt(usuariPos).getNumPrestecsLlargs() >= llistaUsuaris.getAt(usuariPos).getMaxPrestecsLlargs() && esLlarg){
+                throw new BiblioException("L'usuari ha arribat al màxim de prestecs llargs");
+            }
+            if (!esLlarg && llistaUsuaris.getAt(usuariPos).getNumPrestecsNormals() >=
+                    llistaUsuaris.getAt(usuariPos).getMaxPrestecsNormals()) {
+                throw new BiblioException("L'usuari ha arribat al màxim de prestecs normals");
+            }
+            if(esLlarg) {
+                PrestecLlarg prestec = new PrestecLlarg(llistaExemplars.getAt(exemplarPos), llistaUsuaris.getAt(usuariPos));
+                llistaPrestecs.afegir(prestec);
+                llistaUsuaris.getAt(usuariPos).setNumPrestecsLlargs(llistaUsuaris.getAt(usuariPos).getNumPrestecsLlargs() + 1);
 
-        if(llistaUsuaris.getAt(usuariPos).){
+            } else {
+                PrestecNormal prestec = new PrestecNormal(llistaExemplars.getAt(exemplarPos), llistaUsuaris.getAt(usuariPos));
+                llistaPrestecs.afegir(prestec);
+                llistaUsuaris.getAt(usuariPos).setNumPrestecsNormals(llistaUsuaris.getAt(usuariPos).getNumPrestecsNormals() + 1);
 
-        }
+            }
     }
 
     @Override
     public void retornarPrestec(int position) throws BiblioException {
-
+        if(llistaPrestecs.getAt(position).getRetornat()) {
+           throw new BiblioException("El prestec ja ha estat retornat");
+        }
+        llistaPrestecs.getAt(position).setRetornat(true);
+        llistaPrestecs.getAt(position).getExemplar().setDisponible(true);
+        if(llistaPrestecs.getAt(position).tipusPrestec().equals("Normal")) {
+            llistaPrestecs.getAt(position).getUsuari().setNumPrestecsNormals(llistaPrestecs.getAt(position).getUsuari().getNumPrestecsNormals() - 1);
+        } else {
+            llistaPrestecs.getAt(position).getUsuari().setNumPrestecsLlargs(llistaPrestecs.getAt(position).getUsuari().getNumPrestecsLlargs() - 1);
+        }
     }
 
     @Override
     public ArrayList<Prestec> recuperaPrestecs() {
-        return null;
+        return llistaPrestecs.getArrayList();
     }
 
     @Override
     public ArrayList<Prestec> recuperaPrestecsNoRetornats() {
-        return null;
+        ArrayList<Prestec> llistaPrestecsNoRetornats = new ArrayList<>();
+        for(Prestec prestec : llistaPrestecs.getArrayList()) {
+            if(!prestec.getRetornat()) {
+                llistaPrestecsNoRetornats.add(prestec);
+            }
+        }
+        return llistaPrestecsNoRetornats;
     }
 }
